4e8cd509ff08b92f47a39b1c1e47ed529cb6650e
Kazakov500 <kazakov.VADIM.2012@Yandex.ru>
2017-11-15 00:56:10 +0300

    First commit
    All files

---
+++ GeneticOKP/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace GeneticOKP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rtb_Log = new System.Windows.Forms.RichTextBox();
+            this.btn_ShowPool = new System.Windows.Forms.Button();
+            this.btn_Show_good = new System.Windows.Forms.Button();
+            this.tb_i_n = new System.Windows.Forms.TextBox();
+            this.tb_iter_count = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.tb_Step = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_Start.Location = new System.Drawing.Point(114, 303);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(75, 64);
+            this.btn_Start.TabIndex = 0;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(403, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Z1";
+            // 
+            // rtb_Log
+            // 
+            this.rtb_Log.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtb_Log.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtb_Log.Location = new System.Drawing.Point(1, 0);
+            this.rtb_Log.Name = "rtb_Log";
+            this.rtb_Log.Size = new System.Drawing.Size(270, 297);
+            this.rtb_Log.TabIndex = 2;
+            this.rtb_Log.Text = "";
+            // 
+            // btn_ShowPool
+            // 
+            this.btn_ShowPool.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_ShowPool.Location = new System.Drawing.Point(196, 303);
+            this.btn_ShowPool.Name = "btn_ShowPool";
+            this.btn_ShowPool.Size = new System.Drawing.Size(75, 23);
+            this.btn_ShowPool.TabIndex = 21;
+            this.btn_ShowPool.Text = "Show pool";
+            this.btn_ShowPool.UseVisualStyleBackColor = true;
+            this.btn_ShowPool.Click += new System.EventHandler(this.btn_ShowPool_Click);
+            // 
+            // btn_Show_good
+            // 
+            this.btn_Show_good.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Show_good.Location = new System.Drawing.Point(196, 344);
+            this.btn_Show_good.Name = "btn_Show_good";
+            this.btn_Show_good.Size = new System.Drawing.Size(75, 23);
+            this.btn_Show_good.TabIndex = 22;
+            this.btn_Show_good.Text = "Show_Good";
+            this.btn_Show_good.UseVisualStyleBackColor = true;
+            this.btn_Show_good.Click += new System.EventHandler(this.btn_Show_good_Click);
+            // 
+            // tb_i_n
+            // 
+            this.tb_i_n.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.tb_i_n.Location = new System.Drawing.Point(43, 303);
+            this.tb_i_n.Name = "tb_i_n";
+            this.tb_i_n.Size = new System.Drawing.Size(65, 20);
+            this.tb_i_n.TabIndex = 23;
+            this.tb_i_n.Text = "400";
+            // 
+            // tb_iter_count
+            // 
+            this.tb_iter_count.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.tb_iter_count.Location = new System.Drawing.Point(43, 325);
+            this.tb_iter_count.Name = "tb_iter_count";
+            this.tb_iter_count.Size = new System.Drawing.Size(65, 20);
+            this.tb_iter_count.TabIndex = 24;
+            this.tb_iter_count.Text = "10000";
+            // 
+            // label10
+            // 
+            this.label10.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(10, 306);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(9, 13);
+            this.label10.TabIndex = 25;
+            this.label10.Text = "i";
+            // 
+            // label11
+            // 
+            this.label11.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(10, 328);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(21, 13);
+            this.label11.TabIndex = 26;
+            this.label11.Text = "iter";
+            // 
+            // label12
+            // 
+            this.label12.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(10, 350);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(27, 13);
+            this.label12.TabIndex = 28;
+            this.label12.Text = "step";
+            // 
+            // tb_Step
+            // 
+            this.tb_Step.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.tb_Step.Location = new System.Drawing.Point(43, 347);
+            this.tb_Step.Name = "tb_Step";
+            this.tb_Step.Size = new System.Drawing.Size(65, 20);
+            this.tb_Step.TabIndex = 27;
+            this.tb_Step.Text = "4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 373);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.tb_Step);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.tb_iter_count);
+            this.Controls.Add(this.tb_i_n);
+            this.Controls.Add(this.btn_Show_good);
+            this.Controls.Add(this.btn_ShowPool);
+            this.Controls.Add(this.rtb_Log);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Start);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "Genetic Gears";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rtb_Log;
+        private System.Windows.Forms.Button btn_ShowPool;
+        private System.Windows.Forms.Button btn_Show_good;
+        private System.Windows.Forms.TextBox tb_i_n;
+        private System.Windows.Forms.TextBox tb_iter_count;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox tb_Step;
+    }
+}
+
---
+++ GeneticOKP/Form1.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeneticOKP
+{
+    public partial class Form1 : Form
+    {
+        int[] count_of_Z_1 = new int[] { /*14, 15, 16,*/ 17, 18, 19, 20, 21, 22 , 24 ,25, 26 , 28 , 30 , 32 , 34,
+                                        38, 40, 42, 45, 48, 50, 60, 63, 67, 70, 71, 75, 80, 85, 90, 95, 100,
+                                        106, 112, 118, 125, 132, 140, 150,/* 160, 170, 180, 190, 200 */};
+        int[] count_of_Z_ = new int[] { 8, 9, 10, 11, 12, 13 , 23 , 27, 29 , 31, 33 , 35, 37 , 39 ,
+                                        41, 43 , 44 , 46 , 49, 51, 52, 54, 55, 58, 59, 61, 62, 64,
+                                        65,66, 68, 69, 72-74, 78, 81-84, 87, 88, 92, 96, 98,
+                                        102, 103, 104, 105, 108, 109, 110, 114, 115, 116, 117, 120,
+                                        122, 126, 128, 130, 135, 144, 145, 155, 156, 165, 168, 175,
+                                        176, 185, 189, 192, 195, 198 };
+        int[] count_of_Z_1_ = new int[] {14, 15, 16, 17, 18, 19, 20, 21, 22 , 24 ,25, 26 , 28 , 30 , 32 , 34,
+                                        38, 40, 42, 45, 48, 50, 60, 63, 67, 70, 71, 75, 80, 85, 90, 95, 100,
+                                        106, 112, 118, 125, 132, 140, 150, 160, 170, 180, 190, 200,
+                                    8, 9, 10, 11, 12, 13 , 23 , 27, 29 , 31, 33 , 35, 37 , 39 ,
+                                        41, 43 , 44 , 46 , 49, 51, 52, 54, 55, 58, 59, 61, 62, 64,
+                                        65,66, 68, 69, 72, 73, 74, 78, 81, 82, 83, 84, 87, 88, 92, 96, 98,
+                                        102, 103, 104, 105, 108, 109, 110, 114, 115, 116, 117, 120,
+                                        122, 126, 128, 130, 135, 144, 145, 155, 156, 165, 168, 175,
+                                        176, 185, 189, 192, 195, 198 };
+
+
+        List<int[]> Pool = new List<int[]>();
+        int[] GoodRes;
+        float max_Gear;
+
+        int len_individ;
+        bool Solve = false;
+        int step = 0;
+
+        int Pers = 50;
+
+        int Permutation_person_count = 25;
+        int Permutation_count = 25;
+
+        int Mutation_count = 25;
+        int Mutation_persone_count = 25;
+
+        Random rnd = new Random();
+
+        float i;
+        int steps_red;
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            i = (float)Convert.ToDouble(tb_i_n.Text);
+            steps_red = Convert.ToInt32(tb_Step.Text);
+
+            len_individ = steps_red * 2;
+
+
+            for (int i = 0; i < Pers*2; i++)
+            {
+                int[] item = new int[len_individ];
+                for (int j = 0; j < len_individ; j++)
+                {
+                    int ind = rnd.Next(count_of_Z_1.Length);
+                    item[j] = count_of_Z_1[ind];
+                }
+                Pool.Add(item);
+
+            }
+        }
+
+        void Generate()
+        {
+            for (int i = 0; i < Pers; i++)
+            {
+                int[] item = new int[len_individ];
+                for (int j = 0; j < len_individ; j++)
+                {
+                    int ind = rnd.Next(count_of_Z_1.Length);
+                    item[j] = count_of_Z_1[ind];
+                }
+                Pool.Add(item);
+
+            }
+        }
+
+        void Clear()
+        {
+            Pool.Sort(delegate (int[] x, int[] y)
+            {
+                float x_rat = RatingElement(x);
+                float y_rat = RatingElement(y);
+
+                return x_rat.CompareTo(y_rat);
+            });
+
+            Pool.RemoveRange(Pers, Pool.Count - Pers);
+
+
+        }
+
+        private void Permutation()
+        {
+            for (int i = 0; i < Permutation_person_count; i++)
+            {
+                //int[] item = new int[len_individ];// = Pool[rnd.Next(Pool.Count)];
+                int[] item = Pool[rnd.Next(Pool.Count)];
+
+                //Pool[rnd.Next(Pool.Count)].CopyTo(item, 0);
+                for (int j = 0; j < Permutation_count; j++)
+                {
+                    int start_index = rnd.Next(0, len_individ);
+                    int end_index = rnd.Next(0, len_individ);
+
+                    int buff = item[start_index];
+                    item[start_index] = item[end_index];
+                    item[end_index] = buff;
+                }
+                //Pool.Add(item);
+            }
+        }
+        private void Mutation()
+        {
+            for (int i = 0; i < Mutation_persone_count; i++)
+            {
+                int Mutation_person = rnd.Next(0, Pool.Count);
+
+                //int[] item = new int[len_individ];
+                //Pool[rnd.Next(Pool.Count)].CopyTo(item, 0);
+
+                for (int j = 0; j < Mutation_count; j++)
+                {
+                    
+                    int Mutation_index = rnd.Next(0, len_individ);
+
+                    int ind = rnd.Next(count_of_Z_1.Length);
+                    int Mutation = count_of_Z_1[ind];
+
+                    //item[Mutation_index] = Mutation;
+                    Pool[Mutation_person][Mutation_index] = Mutation;
+                }
+
+                //Pool.Add(item);
+            }
+        }
+
+        float getMaxGearD(int[] el)
+        {
+            float maxD = -1;
+
+            //Пом максимальной шестерне
+            //for (int i = 0; i < len_individ; i++)
+            //    if (maxD < (0.4f * el[i])) maxD = 0.4f * el[i];
+
+
+            //По сумме рахмеров
+            float sum = el.Sum();
+
+
+            maxD = sum;
+            
+
+            return maxD;
+        }
+
+
+        void SaveGood()
+        {
+
+            if ((GoodRes == null) || (RatingElement(GoodRes) > RatingElement(Pool[0])))
+            {
+                //if ( (GoodRes != null) && ( getMaxGearD(GoodRes) > getMaxGearD(Pool[0]) ) )
+                //{
+                    GoodRes = new int[len_individ];
+                    Pool[0].CopyTo(GoodRes, 0);
+
+                    float Rat = RatingElement(GoodRes);
+
+                    if (Rat == 0)
+                    {
+                        Solve = true;
+                        AppendText("\n\nSOLVE!!!  ", Color.Green);
+                        WriteGoodRes();
+                    }
+                    step++;
+                //}
+            } else if (RatingElement(GoodRes) == RatingElement(Pool[0]))
+            {
+                if (getMaxGearD(GoodRes) > getMaxGearD(Pool[0]))
+                {
+                    GoodRes = new int[len_individ];
+                    Pool[0].CopyTo(GoodRes, 0);
+                    AppendText("\n\nNEW SOLVE!!! ", Color.Green);
+                    WriteGoodRes();
+                }
+            }
+
+        }
+
+        float RatingElement(int[] el)
+        {
+            //float rat = 1;
+
+            float[] i_nn = new float[steps_red];
+
+            for (int i = 0; i < steps_red; i++)
+                i_nn[i] = (float)el[2 * (i) + 1] / (float)el[2 * i];
+
+
+            /*float i12 = (float)el[1] / (float)el[0];
+            float i34 = (float)el[3] / (float)el[2];
+            float i56 = (float)el[5] / (float)el[4];*/
+
+            //rat = i12 * i34 * i56;
+            float rat = i_nn.Aggregate((p, x) => p *= x);
+
+            return Math.Abs(i - rat);
+
+        }
+        float RatingElement_i(int[] el)
+        {
+            float[] i_nn = new float[steps_red];
+
+            for (int i = 0; i < steps_red; i++)
+                i_nn[i] = (float)el[2 * (i) + 1] / (float)el[2 * i];
+            float rat = i_nn.Aggregate((p, x) => p *= x);
+
+
+            return rat;
+
+        }
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            if ((i != (float)Convert.ToDouble(tb_i_n.Text)) || (steps_red != Convert.ToInt32(tb_Step.Text)))
+            {
+                Pool.Clear();
+                GoodRes = null;
+                max_Gear = -1;
+            }
+            i = (float)Convert.ToDouble(tb_i_n.Text);
+            steps_red = Convert.ToInt32(tb_Step.Text);
+            len_individ = steps_red * 2;
+
+            int iter = Convert.ToInt32(tb_iter_count.Text);
+
+            for (int i = 0; i < iter; i++)
+            {
+                StepFunc();
+            }
+            AppendText("Current result: " + RatingElement_i(GoodRes).ToString() + "        Error: " + (100*(RatingElement_i(GoodRes) - i)/i).ToString() + "% \n");
+        }
+
+        void StepFunc()
+        {
+            Generate();
+
+            //WriteToLogDef();
+
+            Mutation();
+            Permutation();
+
+            Clear();
+
+            //WriteToLogDef();
+
+            SaveGood();
+
+            //WriteToLogDef();
+            
+            //AppendText("Current num: " + Pool.Count.ToString() + "\n");
+
+            //chart1.Series[0].Points.AddY(RatingElement(GoodRes));
+            //chart1.Invalidate();
+        }
+
+
+        public void AppendText(string text, Color? color = null)
+        {
+            rtb_Log.SelectionStart =rtb_Log.TextLength;
+            rtb_Log.SelectionLength = 0;
+            rtb_Log.SelectionColor = color ?? Color.Black;
+            rtb_Log.AppendText(text);
+            rtb_Log.SelectionColor = rtb_Log.ForeColor;
+
+            rtb_Log.Refresh();
+            rtb_Log.Invalidate();
+        }
+
+
+        void WriteToLogDef()
+        {
+            int ii = 0;
+            foreach (var item in Pool)
+            {
+                AppendText("[" + ii.ToString() + "] : ", Color.Black);
+                AppendText("[" + RatingElement_i(item) + "] : ", Color.Black);
+                for (int i = 0; i < len_individ; i++)
+                {
+                    AppendText(item[i].ToString()+ " ", Color.Black);
+                }
+                AppendText("\n", Color.Black);
+
+                
+
+                ii++;
+            }
+            AppendText("------------------------\n\n", Color.Black);
+        }
+
+        void WriteGoodRes()
+        {
+            AppendText("Good : \n", Color.Green);
+            AppendText("[" + RatingElement_i(GoodRes) + "] : \n", Color.Black);
+            for (int i = 0; i < len_individ; i+=2)
+            {
+                AppendText(GoodRes[i].ToString() + " - " + GoodRes[i+1].ToString() + " i = " + ((float)GoodRes[i+1]/(float)GoodRes[i]).ToString() + "\n", Color.Black);
+            }
+            AppendText("\n", Color.Black);
+
+        }
+
+        private void btn_ShowPool_Click(object sender, EventArgs e)
+        {
+            WriteToLogDef();
+        }
+
+        private void btn_Show_good_Click(object sender, EventArgs e)
+        {
+            WriteGoodRes();
+        }
+    }
+}					
